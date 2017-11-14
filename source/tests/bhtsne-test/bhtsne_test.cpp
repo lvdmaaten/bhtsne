0b03a733680a4523a92d5226d4e31da61de8ee47
pascal <pascal.fuehrlich@student.hpi.de>
2017-11-14 10:19:20 +0100

    cmake errors in test folder

---
+++ source/tests/bhtsne-test/bhtsne_test.cpp
@@ -0,0 +1,16 @@
+
+#include <gmock/gmock.h>
+
+
+#include <bhtsne/bhtsne.h>
+
+class bhtsne_test: public testing::Test
+{
+public:
+};
+
+TEST_F(bhtsne_test, sanity_checks)
+{
+    EXPECT_EQ((unsigned int) 0, 0);
+    EXPECT_EQ((unsigned int) 1, 1);
+}
