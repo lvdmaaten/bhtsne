6672394f0f91ad1bdccda25b0499bef944eb6b23
Lukas Wagner <lukas.wagner@student.hpi.de>
2017-11-21 14:44:33 +0100

    refactored svg generation

--- source/tools/result2svg/result2svg.cpp
+++ source/tools/result2svg/result2svg.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,58 +21,55 @@ int main(int argc, char* argv[])
     if(!input.is_open()) fail("Could not open input.");
     cout << "Loaded input." << endl;
 
-    uint32_t N;
-    input.read((char*)&N, 4);
-    cout << "Result contains " << to_string(N) << " samples." << endl;
+    uint32_t sampleCount, dimensionCount;
+    input.read(reinterpret_cast<char*>(&sampleCount), sizeof(sampleCount));
+    input.read(reinterpret_cast<char*>(&dimensionCount), sizeof(dimensionCount));
 
-    uint32_t no_dims;
-    input.read((char*)&no_dims, 4);
-    cout << "Number of dimensions is " << to_string(no_dims) << "." << endl;
-
-    cout << "Set includes " << N << " samples with " << no_dims << " values each." << endl;
-    cout << "Total number of values is " << (N * no_dims) << "." << endl;
+    cout << "Set includes " << sampleCount << " samples with " << dimensionCount << " values each." << endl;
+    cout << "Total number of values is " << (sampleCount * dimensionCount) << "." << endl;
     
-    uint8_t* data = new uint8_t[N * no_dims * sizeof(double)];
-    input.read((char*)data, N * no_dims * sizeof(double));
+    auto data = vector<double>(sampleCount * dimensionCount);
+    input.read(reinterpret_cast<char*>(data.data()), data.size() * sizeof(double));
     cout << "Loaded data." << endl;
 
     input.close();
     cout << "Closed input." << endl;
 
-    double* data_d = new double[N * no_dims];
-    double max = 0;
-    for(int i = 0; i < N * no_dims; i++)
-    {
-        double d = ((double*)data)[i];
-        data_d[i] = d;
-        double a = d < 0 ? -d : d;
-        if(a > max) max = a;
-    }
-    double radius = 0.1;
-    string viewBox = to_string(-max-radius) + " " + to_string(-max-radius) + " " + to_string(2*max+2*radius) + " " + to_string(2*max+2*radius);
+    double extreme = 0;
+    for(int i = 0; i < sampleCount * dimensionCount; i++)
+        extreme = max(extreme, abs(data[i]));
+    double radius = 0.5;
+    double halfwidth = extreme + radius;
+    string viewBox = to_string(-halfwidth) + " " + to_string(-halfwidth) + " " + to_string(2*halfwidth) + " " + to_string(2*halfwidth);
 
-    uint8_t* labels;
+    auto labels = vector<uint8_t>();
     bool useLabels = false;
     if (argc > 2)
     {
         useLabels = true;
-        ifstream input_l;
-        input_l.open(argv[2], ios::in | ios::binary);
-        if (!input_l.is_open()) fail("Could not open labels.");
+        ifstream labelInput;
+        labelInput.open(argv[2], ios::in | ios::binary);
+        if (!labelInput.is_open()) fail("Could not open labels.");
 
-        uint32_t N_l;
-        input_l.read((char*)&N_l, 4);
-        cout << "Labels file contains " << to_string(N_l) << " labels." << endl;
-        if (N_l < N) fail("Not enough labels for result.");
+        uint32_t labelCount;
+        labelInput.read(reinterpret_cast<char*>(&labelCount), sizeof(labelCount));
+        cout << "Labels file contains " << labelCount << " labels." << endl;
+        if (labelCount < sampleCount) fail("Not enough labels for result.");
 
-        labels = new uint8_t[N_l];
-        input_l.read((char*)labels, N_l);
+        labelCount = min(labelCount, sampleCount);
+        labels.resize(labelCount);
+        labelInput.read(reinterpret_cast<char*>(labels.data()), labels.size());
 
-        input_l.close();
+        labelInput.close();
         cout << "Read labels." << endl;
     }
 
-    auto colors = vector<string>{ "red", "blue", "green", "yellow", "magenta", "brown", "orange", "lime", "lightblue", "pink" };
+    uint8_t maxLabel = 0;
+    for (auto label : labels)
+        maxLabel = max(label, maxLabel);
+    auto colors = vector<string>();
+    for (int i = 0; i <= maxLabel; ++i)
+        colors.push_back("hsl(" + to_string(360.0 * i / (maxLabel / 2 + 1)) + ", 100%, " + (i % 2 == 0 ? "25" : "60") + "%)");
 
     ofstream output;
     output.open("result.svg", ios::out | ios::trunc);
@@ -81,13 +80,17 @@ int main(int argc, char* argv[])
     cout << "Wrote header." << endl;
     
     string color = "black";
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < sampleCount; i++)
     {
-        double d0 = ((double*)data)[i*2];
-        double d1 = ((double*)data)[i*2+1];
         if (useLabels)
             color = labels[i] < colors.size() ? colors[labels[i]] : "black";
-        output << "<circle cx=\"" << d0 << "\" cy=\"" << d1 << "\" r=\"0.1\" fill=\"" << color << "\" stroke=\"" << color << "\"/>" << endl;
+
+        output << "<circle "
+            << "cx='" << data[i * 2] << "' "
+            << "cy='" << data[i * 2 + 1] << "' "
+            << "fill='" << color << "' "
+            << "r='" << radius << "' "
+            << "stroke='none' opacity='0.5'/>" << endl;
     }
     output << "</svg>" << endl;
     cout << "Wrote data." << endl;
