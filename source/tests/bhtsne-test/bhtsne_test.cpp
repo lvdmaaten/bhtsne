48ec01fedfb223a5e4132a1db045b32f88c63e31
Lukas Wagner <lukas.wagner@student.hpi.de>
2017-11-28 11:12:49 +0100

    don't used fixture for sanity check test

--- source/tests/bhtsne-test/bhtsne_test.cpp
+++ source/tests/bhtsne-test/bhtsne_test.cpp
@@ -8,7 +8,7 @@ class bhtsne_test: public testing::Test
 public:
 };
 
-TEST_F(bhtsne_test, sanity_checks)
+TEST(sanity_checks)
 {
     EXPECT_EQ((unsigned int) 0, 0);
     EXPECT_EQ((unsigned int) 1, 1);
