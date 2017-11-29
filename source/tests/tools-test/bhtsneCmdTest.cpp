af36b74851b448dc8f722b5a8a763c459c6af09d
pascal <pascal.fuehrlich@student.hpi.de>
2017-11-29 14:39:21 +0100

    use std::string for argString

--- source/tests/tools-test/bhtsneCmdTest.cpp
+++ source/tests/tools-test/bhtsneCmdTest.cpp
@@ -35,21 +35,29 @@ TEST_F(bhtsneCmdTest, CommandLineArgsParsing)
     m_tsne.setOutputFile("result123.csv");
     m_tsne.setRandomSeed(123);
 
-    char argString[] = "./bhtsne_cmd --perplexity 40.123 --gradient-accuracy 2.123 --iterations 4123 --number-of-samples 3123 --output-dimensions 2 --output-file resultbla123.dat --random-seed 321 input_file.dat";
+    //build argv
     auto argv = std::vector<char *>();
-    auto token = strtok(argString, " ");
+    auto argString = std::string{"./bhtsne_cmd "
+            "--perplexity 40.123 "
+            "--gradient-accuracy 2.123 "
+            "--iterations 4123 "
+            "--number-of-samples 3123 "
+            "--output-dimensions 2 "
+            "--output-file another_result.dat "
+            "--random-seed 321 input_file.dat"};
+    auto token = strtok(&argString[0], " ");
     while (token != nullptr) {
         argv.push_back(token);
         token = strtok(nullptr, " ");
     }
 
     applyCommandlineOptions(m_tsne, static_cast<int>(argv.size()), argv.data());
-    free(argString);
+
     EXPECT_EQ(40.123, m_tsne.perplexity()) << "perplexity was not set correctly via commandline option";
     EXPECT_EQ(2.123, m_tsne.gradientAccuracy()) << "gradient-accuracy was not set correctly via commandline option";
     EXPECT_EQ(4123, m_tsne.iterations()) << "iterations was not set correctly via commandline option";
     EXPECT_EQ(3123, m_tsne.getNumberOfSamples()) << "number-of-samples was not set correctly via commandline option";
     EXPECT_EQ(2, m_tsne.outputDimensions()) << "output-dimensions was not set correctly via commandline option";
-    EXPECT_EQ("resultbla123.dat", m_tsne.outputFile()) << "output-file was not set correctly via commandline option";
+    EXPECT_EQ("another_result.dat", m_tsne.outputFile()) << "output-file was not set correctly via commandline option";
     EXPECT_EQ(321, m_tsne.randomSeed()) << "random-seed was not set correctly via commandline option";
 }
--- source/tools/bhtsne_cmd/CommandlineOptions.cpp
+++ source/tools/bhtsne_cmd/CommandlineOptions.cpp
@@ -28,4 +28,4 @@ namespace bhtsne {
         //TODO bool flags
 
     }
-}
+}// namespace bhtsne
