974058772962c9107742a275baab6c6762b3757b
Lukas Wagner <lukas.wagner@student.hpi.de>
2017-12-05 11:02:26 +0100

    fixed name of tsne instance in tests

--- source/tests/bhtsne-test/tsneTest.cpp
+++ source/tests/bhtsne-test/tsneTest.cpp
@@ -30,7 +30,7 @@ class TsneTest : public testing::Test
 protected:
     TsneTest()
     {
-        m_tnse = bhtsne::TSNE();
+        m_tsne = bhtsne::TSNE();
     }
 
     void createTempfile()
@@ -48,7 +48,7 @@ protected:
         remove(tempfile.c_str());
     }
 
-    bhtsne::TSNE m_tnse;
+    bhtsne::TSNE m_tsne;
     std::string tempfile;
     std::ofstream filestream;
     BinaryWriter writer;
@@ -62,53 +62,53 @@ TEST(SanityChecks, Equality)
 
 TEST_F(TsneTest, DefaultValues)
 {
-    EXPECT_EQ(0, m_tnse.randomSeed());
-    EXPECT_EQ(50.0, m_tnse.perplexity());
-    EXPECT_EQ(0.2, m_tnse.gradientAccuracy());
-    EXPECT_EQ(1000, m_tnse.iterations());
-    EXPECT_EQ(2, m_tnse.outputDimensions());
-    EXPECT_EQ(0, m_tnse.getNumberOfSamples());
-    EXPECT_EQ("./result", m_tnse.outputFile());
+    EXPECT_EQ(0, m_tsne.randomSeed());
+    EXPECT_EQ(50.0, m_tsne.perplexity());
+    EXPECT_EQ(0.2, m_tsne.gradientAccuracy());
+    EXPECT_EQ(1000, m_tsne.iterations());
+    EXPECT_EQ(2, m_tsne.outputDimensions());
+    EXPECT_EQ(0, m_tsne.getNumberOfSamples());
+    EXPECT_EQ("./result", m_tsne.outputFile());
 }
 
 TEST_F(TsneTest, RandomSeed)
 {
-    m_tnse.setRandomSeed(1);
-    EXPECT_EQ(1, m_tnse.randomSeed());
-    m_tnse.setRandomSeed(2);
-    EXPECT_EQ(2, m_tnse.randomSeed());
+    m_tsne.setRandomSeed(1);
+    EXPECT_EQ(1, m_tsne.randomSeed());
+    m_tsne.setRandomSeed(2);
+    EXPECT_EQ(2, m_tsne.randomSeed());
 }
 
 TEST_F(TsneTest, Perplexity)
 {
-    m_tnse.setPerplexity(1.0);
-    EXPECT_EQ(1.0, m_tnse.perplexity());
-    m_tnse.setPerplexity(2.0);
-    EXPECT_EQ(2.0, m_tnse.perplexity());
+    m_tsne.setPerplexity(1.0);
+    EXPECT_EQ(1.0, m_tsne.perplexity());
+    m_tsne.setPerplexity(2.0);
+    EXPECT_EQ(2.0, m_tsne.perplexity());
 }
 
 TEST_F(TsneTest, GradientAccuracy)
 {
-    m_tnse.setGradientAccuracy(1.0);
-    EXPECT_EQ(1.0, m_tnse.gradientAccuracy());
-    m_tnse.setGradientAccuracy(2.0);
-    EXPECT_EQ(2.0, m_tnse.gradientAccuracy());
+    m_tsne.setGradientAccuracy(1.0);
+    EXPECT_EQ(1.0, m_tsne.gradientAccuracy());
+    m_tsne.setGradientAccuracy(2.0);
+    EXPECT_EQ(2.0, m_tsne.gradientAccuracy());
 }
 
 TEST_F(TsneTest, Iterations)
 {
-    m_tnse.setIterations(1);
-    EXPECT_EQ(1, m_tnse.iterations());
-    m_tnse.setIterations(2);
-    EXPECT_EQ(2, m_tnse.iterations());
+    m_tsne.setIterations(1);
+    EXPECT_EQ(1, m_tsne.iterations());
+    m_tsne.setIterations(2);
+    EXPECT_EQ(2, m_tsne.iterations());
 }
 
 TEST_F(TsneTest, OutputDimensions)
 {
-    m_tnse.setOutputDimensions(1);
-    EXPECT_EQ(1, m_tnse.outputDimensions());
-    m_tnse.setOutputDimensions(2);
-    EXPECT_EQ(2, m_tnse.outputDimensions());
+    m_tsne.setOutputDimensions(1);
+    EXPECT_EQ(1, m_tsne.outputDimensions());
+    m_tsne.setOutputDimensions(2);
+    EXPECT_EQ(2, m_tsne.outputDimensions());
 }
 
 TEST_F(TsneTest, InputDimensions)
@@ -118,18 +118,18 @@ TEST_F(TsneTest, InputDimensions)
 
 TEST_F(TsneTest, NumberOfSamples)
 {
-    m_tnse.setNumberOfSamples(1);
-    EXPECT_EQ(1, m_tnse.getNumberOfSamples());
-    m_tnse.setNumberOfSamples(2);
-    EXPECT_EQ(2, m_tnse.getNumberOfSamples());
+    m_tsne.setNumberOfSamples(1);
+    EXPECT_EQ(1, m_tsne.getNumberOfSamples());
+    m_tsne.setNumberOfSamples(2);
+    EXPECT_EQ(2, m_tsne.getNumberOfSamples());
 }
 
 TEST_F(TsneTest, OutputFile)
 {
-    m_tnse.setOutputFile("foo.dat");
-    EXPECT_EQ("foo.dat", m_tnse.outputFile());
-    m_tnse.setOutputFile("bar.dat");
-    EXPECT_EQ("bar.dat", m_tnse.outputFile());
+    m_tsne.setOutputFile("foo.dat");
+    EXPECT_EQ("foo.dat", m_tsne.outputFile());
+    m_tsne.setOutputFile("bar.dat");
+    EXPECT_EQ("bar.dat", m_tsne.outputFile());
 }
 
 TEST_F(TsneTest, LoadLegacy)
